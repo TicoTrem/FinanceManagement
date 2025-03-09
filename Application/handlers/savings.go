cdca4adb08ad9c9dc1524ff1d4274df8eefdff53
TicoTrem <ticotremblay@gmail.com>
2025-03-08 19:13:44 -0600

    updated savings to use new utility methods

--- Application/handlers/savings.go
+++ Application/handlers/savings.go
@@ -8,24 +8,28 @@ import (
 )
 
 func HandleSavings() {
-	for {
-		fmt.Printf("Welcome to savings!\nYour amount to contribute this month is: %v\nWhat would you like to do?\n1)\tChange amount saved per month", db.GetSavingsPerMonth())
-		response, exit := utils.GetUserResponse("")
-		if exit {
-			return
-		}
-		if response == "1" {
-			fResponse, exit := utils.GetUserResponseFloat("What would you like to change your monthly savings contribution to?")
-			if exit {
-				return
-			}
-			if fResponse < 0.0 {
-				fmt.Println("The amount you plan to contribute to savings every month should not be a negative number")
-				continue
-			}
-			db.SetSavingsPerMonth(fResponse)
-			return
-		}
-	}
+	fmt.Printf("Welcome to savings!\nYour amount to contribute this month is: %v\n", utils.GetMoneyString(db.GetSavingsPerMonth()))
+	utils.PromptAndHandle("What would you like to do?", []string{"Change amount saved per month", "Send some spending money to savings"}, []func(){handleChangeMonthlySavings, handleAddExtraSavings})
+}
 
+func handleChangeMonthlySavings() {
+	fResponse, exit := utils.GetUserResponseFloat("What would you like to change your monthly savings contribution to?", utils.Positive)
+	if exit {
+		return
+	}
+	if fResponse < 0.0 {
+		fmt.Println("The amount you plan to contribute to savings every month should not be a negative number")
+
+	}
+	db.SetSavingsPerMonth(fResponse)
+	return
+}
+
+func handleAddExtraSavings() {
+	fResponse, exit := utils.GetUserResponseFloat("How much of your spending money would you like to add to savings?", utils.Positive)
+	if exit {
+		return
+	}
+	db.AddTransaction(&db.Transaction{Amount: -fResponse, Date: utils.CurrentTime().AddDate(0, 0, -1), Description: fmt.Sprintf("(Savings) $%v Additional Savings", fResponse)})
+	fmt.Println("Successfully added %v to savings")
 }
